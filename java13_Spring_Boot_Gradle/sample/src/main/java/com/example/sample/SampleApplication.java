5dc1ef7f670b7c90450c2298553d64e377948d9c
miscaminos <sojung1221@gmail.com>
2021-04-04 03:06:08 +0900

    reviewed spring boot configurations

--- java13_Spring_Boot_Gradle/sample/src/main/java/com/example/sample/SampleApplication.java
+++ java13_Spring_Boot_Gradle/sample/src/main/java/com/example/sample/SampleApplication.java
@@ -7,14 +7,12 @@ import org.springframework.context.annotation.ComponentScan;
 
 /** 
  * SampleApplication.java는 spring구성 메인 클래스이다. 
- * 최소한의 설정으로 바로 실행할수있는 application을 만들어준다.
- * 
  * 여기에서 application으 시작할 수 있는 main method가 존재한다.
- * main method는 스프링 부트의 SampleApplication.run() 메소드를 사용하여 스프링
- * 부트 프로젝트 애플리케이션을 실행할 수 있게 한다.
+ * main method는 spring boot의 SampleApplication.run() 메소드를 사용하여
+ * spring boot project application을 실행할 수 있게 한다.
  * 
- * @SpringBootApplication는 스프링 부트의 핵심 어노테이션 - 아래 스프링 부트 어노테이션으로 구성됨.
- * includes the following three Spring boot annotations:
+ * @SpringBootApplication는 스프링 부트의 핵심 어노테이션
+ * includes the following three annotations:
  * @EnableAutoConfiguration : 스프링의 다양한 설정을 자동으로 구성한다.
  * @ComponentScan : 컴포넌트 클래스를 검색하고 검색된 컴포넌트 및 클래스를 스프링 application context에 등록
  * @Configuration : 자바기반 설정 파일(XML설정대신 사용)
